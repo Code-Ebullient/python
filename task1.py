eaa38b965095524264f151102f468215729ca3db
Code-Ebullient <newchassis.pa@gmail.com>
2020-03-26 01:14:57 +0100

    python first task

---
+++ task1.py
@@ -0,0 +1,11 @@
+# Program to calculate area of a circle
+
+# Importing Maths functions
+  
+import math
+from math import pi
+
+r = float(input("Enter the radius of the circle: ")
+area_of_a_circle = pi*(r**2)
+print("The area of the circle is: %.3f " % area_of_a_circle)
+
