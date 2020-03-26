36f03f1352816de804362ab33ae9547efe509b62
Ebullient <38101674+Code-Ebullient@users.noreply.github.com>
2020-03-26 02:22:05 +0100

    Update task1.py

--- task1.py
+++ task1.py
@@ -5,7 +5,7 @@
 import math
 from math import pi
 
-r = float(input("Enter the radius of the circle: ")
+r = float(input("Enter the radius of the circle: "))
 area_of_a_circle = pi*(r**2)
 print("The area of the circle is: %.3f " % area_of_a_circle)
 
